501ed011e4b0df1097ad50f25ba6b81cb2dfb96c
Kerem Kaya <keremsah_kaya@hotmail.com>
2020-10-28 00:35:06 +0100

    answerBooklets

--- RecommendationsSystemSqlSyntax.sql
+++ RecommendationsSystemSqlSyntax.sql
@@ -1,27 +1,27 @@
 -- step 1
 
 alter table movies
-add lexemesX; -- < replace x with column name 
+add lexemesDirector tsvector; -- < replace x with column name 
 
 update movies
-set lexemesX = to_tsvector(x); -- replace the X with with column name
+set lexemesDirector = to_tsvector(Director); -- replace the X with with column name
 
 -- step 2
 
 select url from movies
-where lexemesX @@ to_tsquery('y'); -- replace x with column, replace y with search key
+where lexemesDirector @@ to_tsquery('David'); -- replace x with column, replace y with search key
 
 -- step 3
 
 alter table movies
-add rank float4;
+add rank2 float4;
 
 update movies
-set rank = ts_rank(lexemesX, plainto_tsquery( --replace x
+set rank = ts_rank(lexemesDirector, plainto_tsquery( --replace x
 (
-select col.name from table where col = '?' -- replace col with names and indicate search criteria at ?
+select Summary from table where url Like 'fight' -- replace col with names and indicate search criteria at ?
 )
 ));
 
-create table recommendationsBasedonXfield as --replace x 
-select col1, col2, from table where col > x order by x desc limit 50; -- replace col1 and col2 replace other values as well.
+create table recommendationsBasedonDirectorfield as --replace x 
+select url, rank2 from table where rank2 > 0.1 order by rank2 desc limit 50; -- replace col1 and col2 replace other values as well.
