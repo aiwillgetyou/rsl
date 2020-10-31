87fb9dd7206f2ef291bb4d2a97e84d40cc086120
Kerem Kaya <keremsah_kaya@hotmail.com>
2020-10-31 18:34:01 +0100

    if not exists

--- RecommendationsSummaryArrival.sql
+++ RecommendationsSummaryArrival.sql
@@ -3,7 +3,7 @@
 -- step 1
 
 alter table movies
-add lexemesSummary tsvector;
+add column if not exists lexemesSummary tsvector;
 
 update movies
 set lexemesSummary = to_tsvector(summary);
@@ -16,7 +16,7 @@ where lexemesSumary @@ to_tsquery('arrival');
 -- step 3
 
 alter table movies
-add rankSummary float4;
+add column if not exists rankSummary float4;
 
 update movies
 set rankSummary = ts_rank(lexemesSummary, plainto_tsquery(
@@ -25,8 +25,7 @@ select Summary from movies where url = 'arrival'
 )
 ));
 
-create table recommendationsSummaryArrival as 
+create table if not exists  recommendationsSummaryArrival as 
 select url, rankSummary from movies where rankSummary > 0.5 order by rankSummary desc limit 50;
 
-\copy (select * from recommendationsSummaryArrival) to
-'/home/pi/rsl/topRecommendationsArrivalSummary.csv' with csv;
+\copy (select * from recommendationsSummaryArrival) to '/home/pi/rsl/topRecommendationsArrivalSummary.csv' with csv;
--- RecommendationsTitle300.sql
+++ RecommendationsTitle300.sql
@@ -3,7 +3,7 @@
 -- step 1
 
 alter table movies
-add lexemesTitle tsvector;
+add column if not exists lexemesTitle tsvector;
 
 update movies
 set lexemesTitle = to_tsvector(Title);
@@ -11,12 +11,12 @@ set lexemesTitle = to_tsvector(Title);
 -- step 2
 
 select url from movies
-where lexemesTitle@@ to_tsquery('300');
+where lexemesTitle @@ to_tsquery('300');
 
 -- step 3
 
 alter table movies
-add rankTitle float4;
+add column if not exists rankTitle float4;
 
 update movies
 set rankTitle = ts_rank(lexemesTitle , plainto_tsquery(
@@ -25,7 +25,7 @@ select title from movies where url = '300'
 )
 ));
 
-create table recommendationsTitle300 as 
+create table if not exists recommendationsTitle300 as 
 select url, rankTitle from movies where rankTitle > 0.1 order by rankTitle desc limit 50;
 
 \copy (select * from recommendationsTitle300) to '/home/pi/rsl/topRecommendations300Title.csv' with csv;
